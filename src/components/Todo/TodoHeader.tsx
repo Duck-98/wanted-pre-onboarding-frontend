9a9e0f18360901daa677271b83fbbacb46960bbe
duck <godqhr2256@gmail.com>
2022-12-07 16:34:55 +0900

    feat: 로그아웃 기능

--- src/components/Todo/TodoHeader.tsx
+++ src/components/Todo/TodoHeader.tsx
@@ -1,7 +1,13 @@
+import { useTodoState } from "context/todoContext";
 import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import { currentTime } from "utils/today";
+import { useNavigate } from "react-router-dom";
+
 const TodoHeader = () => {
+  const todos = useTodoState();
+  const navigate = useNavigate();
+  const filterData = todos.filter((todo) => todo.isCompleted === false);
   const [nowTimes, setNowTimes] = useState(currentTime().slice(13, 19));
   useEffect(() => {
     const timer = setInterval(() => {
@@ -11,6 +17,10 @@ const TodoHeader = () => {
       clearInterval(timer);
     };
   }, []);
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    navigate("/");
+  };
   return (
     <div>
       <HeaderContainer>
@@ -19,16 +29,37 @@ const TodoHeader = () => {
           <h1>{currentTime().slice(5, 8)}</h1>
           <h1>{currentTime().slice(8, 10)}</h1>
           <h1>{currentTime().slice(10, 13)}</h1>
+          <Button onClick={handleLogout}>로그아웃</Button>
         </div>
         <div className="sub-container">
           <HeaderSpan>{nowTimes}</HeaderSpan>
-          <HeaderSpan>해야 할 일 3개</HeaderSpan>
+          {filterData.length === 0 ? (
+            <HeaderSpan>오늘 할 일을 완료했습니다!</HeaderSpan>
+          ) : (
+            <HeaderSpan>해야 할 일 {filterData.length}개</HeaderSpan>
+          )}
         </div>
       </HeaderContainer>
       <Divider />
     </div>
   );
 };
+
+const Button = styled.button`
+  width: 70px;
+  height: 20px;
+  cursor: pointer;
+  background: #9f78b8;
+  &:hover {
+    background: #b088c9;
+  }
+  &:active {
+    background: #b088c9;
+  }
+  border: none;
+  color: white;
+  border-radius: 5px;
+`;
 
 const HeaderContainer = styled.div`
   padding: 20px 32px 25px 32px;
@@ -47,6 +78,7 @@ const HeaderContainer = styled.div`
 
 const HeaderSpan = styled.span`
   font-size: 18px;
+  font-weight: bold;
 `;
 
 const Divider = styled.div`
